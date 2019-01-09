01131ce8f462f7088752757815a8cdada6915b3b
André Ribeiro <andre.ribeiro@ebankit.com>
2019-01-09 16:19:54 +0000

    Add project files.

---
+++ EstudoChatBot.Api/Bots/SingleBot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+namespace EstudoChatBot.Api.Bots
+{
+    public class SingleBot : IBot
+    {
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (turnContext == null)
+            {
+                throw new ArgumentNullException("Context is null");
+            }
+
+            if (turnContext.Activity.Type == ActivityTypes.Message)
+            {
+                await turnContext.SendActivityAsync(turnContext.Activity.Text);
+            }
+        }
+    }
+}
---
+++ EstudoChatBot.Api/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EstudoChatBot.Api
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddBot<Bots.SingleBot>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles()
+                .UseStaticFiles()
+                .UseBotFramework();
+
+        }
+    }
+}
